3002fc30ab7d5f09fda8708be604599497d7242d
Kiran <116376429+kiranvshah@users.noreply.github.com>
2024-07-25 22:25:40 +0100

    add some css

--- client/src/index.ts
+++ client/src/index.ts
@@ -2,13 +2,25 @@
 const memoryTableBody = document.getElementById(
 	"memoryTbody",
 ) as HTMLTableElement;
+const memoryContentsSpans: HTMLSpanElement[] = [];
 
 for (let rowNumber = 0; rowNumber < 10; rowNumber++) {
 	const row = document.createElement("tr");
 	for (let colNumber = 0; colNumber < 10; colNumber++) {
 		const memoryAddress = rowNumber * 10 + colNumber;
 		const cell = document.createElement("td");
-		cell.innerText = memoryAddress.toString();
+
+		const memoryAddressLabel = document.createElement("span");
+		memoryAddressLabel.classList.add("memory-address");
+		memoryAddressLabel.innerText = memoryAddress.toString();
+
+		const memoryContentsSpan = document.createElement("span");
+		memoryContentsSpan.innerText = "0";
+		memoryContentsSpans.push(memoryContentsSpan);
+
+		cell.appendChild(memoryAddressLabel);
+		cell.appendChild(document.createElement("br"));
+		cell.appendChild(memoryContentsSpan);
 		row.appendChild(cell);
 	}
 	memoryTableBody.appendChild(row);
