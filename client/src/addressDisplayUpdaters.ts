60835e40738bae5c23a1f5aa47c0d9827ee90a20
Kiran <116376429+kiranvshah@users.noreply.github.com>
2024-07-26 17:38:49 +0100

    add register display updaters

--- client/src/addressDisplayUpdaters.ts
+++ client/src/addressDisplayUpdaters.ts
@@ -5,3 +5,27 @@ export function updateMemoryLocation(
 ) {
 	memoryContentsSpans[address].innerText = value.toString().padStart(3, "0");
 }
+
+export function updateProgramCounter(value: number) {
+	const pcSpan = document.getElementById(
+		"programCounterValueSpan",
+	) as HTMLSpanElement;
+	pcSpan.innerText = value.toString();
+}
+
+export function updateAccumulator(value: number) {
+	const accSpan = document.getElementById(
+		"accumulatorValueSpan",
+	) as HTMLSpanElement;
+	accSpan.innerText = value.toString();
+}
+
+export function updateMar(value: number) {
+	const marSpan = document.getElementById("marValueSpan") as HTMLSpanElement;
+	marSpan.innerText = value.toString();
+}
+
+export function updateMdr(value: number) {
+	const mdrSpan = document.getElementById("mdrValueSpan") as HTMLSpanElement;
+	mdrSpan.innerText = value.toString();
+}
