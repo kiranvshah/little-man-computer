236a964740044b61acc9279219d3e1fb7c0a6bd7
kiran <116376429+kiranvshah@users.noreply.github.com>
2024-09-17 11:46:51 +0000

    addressDisplayUpdaters docstrings

--- client/src/addressDisplayUpdaters.ts
+++ client/src/addressDisplayUpdaters.ts
@@ -1,3 +1,9 @@
+/**
+ * Updates the value displayed in a given location in RAM.
+ * @param {number} address The address of the RAM location to update (0-99).
+ * @param {number} value The value to change the display to (0-999). 
+ * @param {HTMLSpanElement[]} memoryContentsSpans The array of <span> elements that contain the memory contents.
+ */
 export function updateMemoryLocation(
 	address: number,
 	value: number,
@@ -6,6 +12,10 @@ export function updateMemoryLocation(
 	memoryContentsSpans[address].innerText = value.toString().padStart(3, "0");
 }
 
+/**
+ * Updates the value displayed in the program counter.
+ * @param {number} value The value to display (0-99).
+ */
 export function updateProgramCounter(value: number) {
 	const pcSpan = document.getElementById(
 		"programCounterValueSpan",
@@ -13,6 +23,10 @@ export function updateProgramCounter(value: number) {
 	pcSpan.innerText = value.toString();
 }
 
+/**
+ * Updates the value displayed in the accumulator.
+ * @param {number} value The value to display (0-999). 
+ */
 export function updateAccumulator(value: number) {
 	const accSpan = document.getElementById(
 		"accumulatorValueSpan",
@@ -20,11 +34,19 @@ export function updateAccumulator(value: number) {
 	accSpan.innerText = value.toString();
 }
 
+/**
+ * Updates the value displayed in the MAR.
+ * @param {number} value The value to display (0-99).
+ */
 export function updateMar(value: number) {
 	const marSpan = document.getElementById("marValueSpan") as HTMLSpanElement;
 	marSpan.innerText = value.toString();
 }
 
+/**
+ * Updates the value displayed in the MDR.
+ * @param {number} value The value to display (0-999).
+ */
 export function updateMdr(value: number) {
 	const mdrSpan = document.getElementById("mdrValueSpan") as HTMLSpanElement;
 	mdrSpan.innerText = value.toString();
