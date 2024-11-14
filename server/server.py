fe8b4249c29f0a952792684ecce81e3bafe1e6a9
kiran <116376429+kiranvshah@users.noreply.github.com>
2024-11-14 15:01:20 +0000

    add todo

--- server/server.py
+++ server/server.py
@@ -10,6 +10,7 @@ def post_compile():
     print(request.get_data())
     print(request.mimetype)
     print(request.is_json)
+    # todo: request.is_json appearing as true but request.get_json() throwing 400 error
     # if request.is_json:
     #     print("is json, getting body")
     #     body = request.get_json()
