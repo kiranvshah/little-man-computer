76c884a2eb8bdf10845e18831ab0c2cac96cf8cc
kiran <116376429+kiranvshah@users.noreply.github.com>
2024-11-14 15:00:26 +0000

    fix cors, respond to requests

--- server/server.py
+++ server/server.py
@@ -1,12 +1,19 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import flask_cors
 
 
 app = Flask(__name__)
+flask_cors.CORS(app)
 
 @app.post("/api/compile")
 def post_compile():
     print(request.get_data())
-    if request.is_json:
-        body = request.get_json()
-        print(body)
+    print(request.mimetype)
+    print(request.is_json)
+    # if request.is_json:
+    #     print("is json, getting body")
+    #     body = request.get_json()
+    #     print("body", body)
+    response = jsonify({'received': True})
+    return response
     return "Expected JSON request", 415 # Unsupported Media Type
