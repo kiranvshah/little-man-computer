88e337f338a2aeba780bbac2e164eaaeae7ca021
kiran <116376429+kiranvshah@users.noreply.github.com>
2024-11-14 18:55:54 +0000

    add event listener to assemble btn

--- client/src/index.ts
+++ client/src/index.ts
@@ -48,3 +48,8 @@ async function assembleCode() {
 		mode: "cors",
 	});
 }
+document.addEventListener("DOMContentLoaded", () =>
+	(
+		document.getElementById("assembleButton") as HTMLButtonElement
+	).addEventListener("click", assembleCode),
+);
