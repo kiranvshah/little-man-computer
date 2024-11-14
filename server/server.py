55ef36ffc6ffc4acc0c9cf58d6a4e5dc52d49d81
kiran <116376429+kiranvshah@users.noreply.github.com>
2024-11-14 15:05:48 +0000

    json response working!

--- server/server.py
+++ server/server.py
@@ -10,11 +10,9 @@ def post_compile():
     print(request.get_data())
     print(request.mimetype)
     print(request.is_json)
-    # todo: request.is_json appearing as true but request.get_json() throwing 400 error
-    # if request.is_json:
-    #     print("is json, getting body")
-    #     body = request.get_json()
-    #     print("body", body)
-    response = jsonify({'received': True})
-    return response
+    if request.is_json:
+        body = request.get_json()
+        print("body", body)
+        response = jsonify({'received': True})
+        return response
     return "Expected JSON request", 415 # Unsupported Media Type
