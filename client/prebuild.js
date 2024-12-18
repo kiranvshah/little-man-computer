3210f366c65a1d52d6b9cb4527c04801a50d3c7a
kiran <116376429+kiranvshah@users.noreply.github.com>
2024-12-18 11:30:54 +0000

    fix prebuild,js

--- client/prebuild.js
+++ client/prebuild.js
@@ -4,5 +4,7 @@ const path = require("path");
 const indexTsPath = path.join(__dirname, "src", "index.ts");
 const serverUrl = process.env.SERVER_URL || "http://localhost:5000";
 
-let contents = fs.readFileSync(indexTsPath);
+let contents = fs.readFileSync(indexTsPath, "utf-8");
 contents = contents.replace(/%%SERVER_URL%%/g, serverUrl);
+
+fs.writeFileSync(indexTsPath, contents)
