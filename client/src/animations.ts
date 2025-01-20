0362917ea7b4841487254c3f294012962507c0c7
kiran <116376429+kiranvshah@users.noreply.github.com>
2025-01-20 17:32:09 +0000

    fix animations (fixes #47)

--- client/src/animations.ts
+++ client/src/animations.ts
@@ -1,15 +1,16 @@
 export function animateTranslation(endId: string) {
 	const dot = document.getElementById("dot") as HTMLDivElement;
+	const originParent = dot.parentElement!;
 	const destinationParent = document.getElementById(endId) as HTMLDivElement;
 
 	const startRect = dot.getBoundingClientRect();
-
-	// move dot
-	destinationParent.appendChild(dot);
+	destinationParent.appendChild(dot); // move dot to destination in order to get final coords
 	const endRect = dot.getBoundingClientRect();
+	originParent.appendChild(dot); // move dot back to origin before animation begins
 
-	const dx = startRect.left - endRect.left + "px";
-	const dy = startRect.top - endRect.top + "px";
+	const dx = endRect.left - startRect.left + "px";
+	const dy = endRect.top - startRect.top + "px";
+
 	dot.style.setProperty("--dx", dx);
 	dot.style.setProperty("--dy", dy);
 	dot.classList.add("mover");
