2f93b047bebe728ce64442a5081d9193e254b88d
kiran <116376429+kiranvshah@users.noreply.github.com>
2025-03-14 15:15:41 +0000

    remove __name__ == "__main__" code from computer.py

--- server/computer.py
+++ server/computer.py
@@ -299,33 +299,3 @@ class Computer:
             all_results.append(result)
 
         return all_results
-
-if __name__ == "__main__":
-    from compile_assembly import compile_assembly
-    compile_assembly_result = compile_assembly("""// store an input
-// at position first
-INP
-STA first
-// store an input at
-// position second
-INP
-STA second
-// load the first value
-LDA first
-// subtract the
-// second
-SUB second
-// output the difference
-// and halt execution
-OUT
-HLT
-
-// use the DAT instruction to create two 'variables' called first and second, and set them both to 0
-first DAT 000
-second DAT 000""")
-    comp = Computer(compile_assembly_result["memory_and_registers"])
-    print(comp.step())
-    print(comp.step())
-    print(comp.step())
-    print(comp.step())
-    print(comp.step())
